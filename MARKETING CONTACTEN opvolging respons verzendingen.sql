cc55811aadfc1e19edac6dfb08970a4825db0944
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2023-01-25 13:31:43 +0100

    Update MARKETING CONTACTEN opvolging respons verzendingen.sql

--- MARKETING CONTACTEN opvolging respons verzendingen.sql
+++ MARKETING CONTACTEN opvolging respons verzendingen.sql
@@ -1,10 +1,11 @@
 -- unieke "extra info" per "status/fase" voor specifiek "status/fase"
-SELECT DISTINCT mcf.id, mcf.name, mei.info
+SELECT DISTINCT mcf.id, mcf.name, mei.info, mei.datetime::date
 FROM res_partner p
 	JOIN res_crm_marketing_contact_history mch ON mch.history_id = p.id
 	JOIN res_crm_marketing_contact_fase mcf ON mcf.id = mch.contact_fase
 	JOIN res_crm_marketing_extra_info mei ON mei.info_id = mch.history_id AND mei.datetime::date = mch.datetime::date
-WHERE mcf.id IN (24,25,26)
+--WHERE mcf.id IN (24,25,26)
+WHERE mcf.id = 24
 ORDER by mcf.id
 ------------------------------------
 SELECT DISTINCT mch.history_id, mcf.name, mei.info
@@ -35,7 +36,7 @@ FROM res_partner p
 -----------------------------------
 -- opvolging response LEDENSERVICE
 -----------------------------------
-SELECT sq1.*, p.id, p.name, p.membership_state, p.active,
+SELECT sq1.partner_id, sq1.status, sq1.info, u.login, sq1.date, p.id, p.name, p.membership_state, p.active,
 	p.membership_start Lidmaatschap_startdatum, 
 	p.membership_stop Lidmaatschap_einddatum,  
 	p.membership_pay_date betaaldatum,
@@ -43,13 +44,15 @@ SELECT sq1.*, p.id, p.name, p.membership_state, p.active,
 	p.membership_end recentste_einddatum_lidmaatschap,
 	p.membership_cancel membership_cancel
 FROM res_partner p
-	JOIN (SELECT DISTINCT p.id partner_id, mcf.name status, mei.info, /*mei.datetime::date,*/ mch.datetime::date date
+	JOIN (SELECT DISTINCT p.id partner_id, mcf.name status, mei.info, mch.author, /*mei.datetime::date,*/ mch.datetime::date date
 					FROM res_partner p
 						JOIN res_crm_marketing_contact_history mch ON mch.history_id = p.id
 						JOIN res_crm_marketing_contact_fase mcf ON mcf.id = mch.contact_fase
 						JOIN res_crm_marketing_extra_info mei ON mei.info_id = mch.history_id AND mei.datetime::date = mch.datetime::date
-					WHERE mcf.id = 25) sq1
-				ON sq1.partner_id = p.id				
+					WHERE mcf.id = 25 AND mei.info LIKE '%lternatief%') sq1
+				ON sq1.partner_id = p.id
+	JOIN res_users u ON u.id = sq1.author	
+ORDER BY sq1.date
 -----------------------------
 -- opvolging response GIFTEN
 -----------------------------
@@ -139,7 +142,7 @@ SELECT DISTINCT sq1.* ,
 					JOIN res_crm_marketing_contact_history mch ON mch.history_id = p.id
 					JOIN res_crm_marketing_contact_fase mcf ON mcf.id = mch.contact_fase
 					JOIN res_crm_marketing_extra_info mei ON mei.info_id = mch.history_id AND mei.datetime::date = mch.datetime::date
-				WHERE mcf.id = 24 ) sq1
+				WHERE mcf.id = 24 AND mch.datetime::date = '2022-10-31' ) sq1
 			ON sq1.partner_id = p.id
 
 		JOIN res_company rc ON aml.company_id = rc.id 
