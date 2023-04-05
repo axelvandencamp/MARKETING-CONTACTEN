5750ada614d1a90b0047d32b3dc96ddcb879b738
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2023-04-05 10:46:04 +0200

    Update MARKETING_CONTACTEN_basis.sql

--- MARKETING_CONTACTEN_basis.sql
+++ MARKETING_CONTACTEN_basis.sql
@@ -3,13 +3,16 @@ SELECT p.id, tr.* FROM crm_marketing_partner_type_rel tr
 WHERE p.active = false AND tr.name = 2
 SELECT * FROM crm_marketing_partner_type_rel
 SELECT * FROM res_crm_marketing_partner_type
+-- SELECT id, name FROM res_crm_marketing_partner_type
 SELECT * FROM res_crm_marketing_contact_history
 SELECT * FROM res_crm_marketing_contact_fase
 --SELECT * FROM crm_marketing_sector_rel
 SELECT * FROM res_crm_marketing_extra_info -- WHERE info_id = 21362
 SELECT * FROM res_crm_marketing_sector
 
-SELECT * FROM res_users u WHERE u.login like 'linsay%'
+SELECT * FROM res_users u WHERE u.login like 'ward%'
+
+SELECT id, name FROM res_crm_marketing_partner_type
 
 
 SELECT * FROM res_crm_marketing_contact_history mch WHERE mch.partner_type = 2
@@ -22,14 +25,20 @@ SELECT * FROM
 		JOIN res_crm_marketing_partner_type mpt ON mpt.id = mptr.name
 		LEFT OUTER JOIN res_crm_marketing_contact_history mch ON mch.history_id = p.id
 		LEFT OUTER JOIN res_crm_marketing_contact_fase mcf ON mcf.id = mch.contact_fase
+	 	--JOIN res_users u ON u.id = mch.write_uid AND mch.write_uid = 315 AND mch.write_date >= '2023-03-08'  -- aanpassing obv user_id
+	WHERE mch.author = 548 AND mch.write_date >= '2022-03-09' -- aanpassing obv auteur
 	UNION ALL
-	SELECT p.id, p.name, mptr.name p_type_id, mpt.name partner_type, 'info' _type, mei.datetime::date _date, COALESCE(mei.info,'n/a') info
+	SELECT p.id, p.name, mptr.name p_type_id, mpt.name partner_type, 'info' _type, mei.datetime::date _date, 
+	 	REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(COALESCE(mei.info,'n/a'),chr(9),';'),chr(10),';'),chr(11),';'),chr(12),';'),chr(13),';') info
 	FROM res_partner p
 		JOIN crm_marketing_partner_type_rel mptr ON mptr.partner_type_id = p.id
 		JOIN res_crm_marketing_partner_type mpt ON mpt.id = mptr.name
-		LEFT OUTER JOIN res_crm_marketing_extra_info mei ON mei.info_id = p.id) SQ1
+		LEFT OUTER JOIN res_crm_marketing_extra_info mei ON mei.info_id = p.id
+		--JOIN res_users U ON u.id = mei.write_uid AND mei.write_uid = 315 AND mei.write_date >= '2023-03-08'  -- aanpassing obv user_id
+	WHERE mei.author = 548 AND mei.write_date >= '2022-03-09' -- aanpassing obv auteur
+	) SQ1
 	JOIN marketing._crm_partnerinfo() SQ2 ON SQ2.partner_id = SQ1.id
-WHERE NOT(info = 'n/a') AND p_type_id = 1
+WHERE NOT(info = 'n/a') --AND p_type_id = 1
 ORDER BY SQ1.id, SQ1._date DESC	
 
 -- ophalen verzendlijst --
