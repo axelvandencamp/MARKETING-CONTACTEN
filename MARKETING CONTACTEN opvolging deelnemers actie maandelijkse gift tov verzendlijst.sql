97469eb865a8ffccd131dcefe6941b457f494596
Axel Van den Camp <32923828+axelvandencamp@users.noreply.github.com>
2025-08-27 11:05:19 +0200

    Update MARKETING CONTACTEN opvolging deelnemers actie maandelijkse gift tov verzendlijst.sql

--- MARKETING CONTACTEN opvolging deelnemers actie maandelijkse gift tov verzendlijst.sql
+++ MARKETING CONTACTEN opvolging deelnemers actie maandelijkse gift tov verzendlijst.sql
@@ -1,5 +1,7 @@
 -- CONTROLE partner_id's originele verzendlijst tov in marketing_contacten geregistreerde deelnemers aan actie
-SELECT *
+SELECT sq1.partner_id vzl_partner_id, sq1.name vzl_name, sq1.date vzl_datum, sq1.info vzl_info,
+	sq2.partner_id r_partner_id, COALESCE(sq2.name,'') r_name, sq2.date r_datum, 
+	REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(COALESCE(sq2.info,''),chr(9),''),chr(10),''),chr(11),''),chr(12),''),chr(13),'') r_info
 FROM
 	(
 	SELECT DISTINCT p.id partner_id, mcf.name, mei.info, mch.datetime::date date
